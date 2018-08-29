78d21d567fd98ff204c6b7292679093790940e18
hoangviet <hoangviet1312@gmail.com>
2018-08-29 08:54:34 +0700

    add service

---
+++ src/main/java/com/codegym/cms/service/BlogService.java
@@ -0,0 +1,20 @@
+package com.codegym.cms.service;
+
+import com.codegym.cms.model.Blog;
+import com.codegym.cms.model.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface BlogService {
+    Page<Blog> findAll(Pageable pageable);
+
+    Blog findById(Long id);
+
+    void save(Blog blog);
+
+    void remove(Long id);
+
+    Page<Blog> findAllByBlogNameContaining(String blogName, Pageable pageable);
+
+    Iterable<Blog> findAllByCategory(Category category);
+}
