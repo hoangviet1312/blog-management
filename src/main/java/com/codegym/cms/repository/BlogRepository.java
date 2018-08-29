c29c5c5dd0a84c1b54d6dcf0d380dfdf649d57ae
hoangviet <hoangviet1312@gmail.com>
2018-08-29 08:54:15 +0700

    add repository

---
+++ src/main/java/com/codegym/cms/repository/BlogRepository.java
@@ -0,0 +1,13 @@
+package com.codegym.cms.repository;
+
+import com.codegym.cms.model.Blog;
+import com.codegym.cms.model.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface BlogRepository extends PagingAndSortingRepository<Blog, Long> {
+    Iterable<Blog> findAllByCategory(Category category);
+
+    Page<Blog> findAllByBlogNameContaining(String blogName, Pageable pageable);
+}
